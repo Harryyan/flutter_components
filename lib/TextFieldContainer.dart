cbacfd3e707b1fb5768c4a85bdc28fb7fd07c8f7
harry yan <harryyan1238@gmail.com>
2020-12-21 21:58:23 +1300

    Init

---
+++ lib/TextFieldContainer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:emerce_components/constant//constants.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+
+  const TextFieldContainer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 给定Context数据的媒体信息:
+    // 例如，window宽高/横竖屏/像素密度比 等信息
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: kPrimaryLightColor,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: child,
+    );
+  }
+}
